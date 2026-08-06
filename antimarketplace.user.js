0abe51991205f408e9c0dd0c06c0a831a7370489
kane-c <kane-c@users.noreply.github.com>
2026-08-07 09:57:22 +1000

    Apply rules when the URL changes (SPA)

--- antimarketplace.user.js
+++ antimarketplace.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Anti-marketplace
 // @namespace    github.com/kane-c/usercss
-// @version      1.3.0
+// @version      1.4.0
 // @description  Excludes Marketplace results from Australian retailers. Companion of the adblock filter list: https://github.com/danielnixon/anti-marketplace
 // @author       @kane-c
 // @downloadURL  https://raw.githubusercontent.com/kane-c/usercss/refs/heads/main/antimarketplace.user.js
@@ -24,25 +24,55 @@
   };
 
   const url = new URL(window.location.href);
-
   const [, site] = url.hostname.split('.');
   const [param, expectedValues] = siteRules[site];
   const values = Array.isArray(expectedValues) ? expectedValues : [expectedValues];
-  const currentValues = url.searchParams.getAll(param);
-  let changed = false;
-  for (const expectedValue of values) {
-    if (!currentValues.includes(expectedValue)) {
-      url.searchParams.append(param, expectedValue);
+
+  function checkUrl(url) {
+    const currentValues = url.searchParams.getAll(param);
+    let changed = false;
+
+    for (const expectedValue of values) {
+      if (!currentValues.includes(expectedValue)) {
+        url.searchParams.append(param, expectedValue);
+        changed = true;
+      }
+    }
+
+    if (site === 'woolworths' && !url.searchParams.has('sortBy')) {
+      // Sort by unit price by default
+      url.searchParams.append('sortBy', 'CUPAsc');
       changed = true;
     }
+
+    if (changed) {
+      return url.toString();
+    }
+
+    return undefined;
   }
 
-  if (changed) {
-    window.location.replace(url.toString());
+  const newUrl = checkUrl(url);
+
+  if (newUrl) {
+    window.location.replace(url);
     return;
   }
 
   if (site === 'woolworths') {
+    // Catch URL changes
+    window.history.pushState = new Proxy(window.history.pushState, {
+      apply: (target, thisArg, argArray) => {
+        const pathAndSearch = argArray[2];
+        const newUrl = checkUrl(new URL(url.origin + pathAndSearch));
+        if (newUrl) {
+          argArray[2] = newUrl;
+        }
+
+        return target.apply(thisArg, argArray);
+      },
+    });
+
     // Fix Cmd+click
     window.addEventListener('click', (event) => {
       if (event.metaKey || event.ctrlKey || event.shiftKey || event.altKey) {
